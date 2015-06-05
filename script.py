ffb855f252dfb5fc4e6520a73cfa45af7793afca
Simon Flachs <simonflachs@Simons-MacBook-Pro.local>
2015-06-05 14:20:46 +0900

    hyperparameter testing

--- script.py
+++ script.py
@@ -36,6 +36,9 @@ features = ['season', 'holiday', 'workingday', 'weather', 'temp', 'atemp', 'humi
 for col in ['casual', 'registered', 'count']:
 	train['log-' + col ] = train[col].apply(lambda x: np.log1p(x))
 
+
+
+"""
 clf = ensemble.GradientBoostingRegressor(n_estimators=200, max_depth=3)
 clf.fit(train[features], train['log-count'])
 result = clf.predict(test[features])
@@ -43,6 +46,29 @@ result = np.expm1(result)
 
 df = pd.DataFrame({'datetime':test['datetime'], 'count':result})
 df.to_csv('results1.csv', index= False, columns=['datetime','count'])
+"""
+
+#Split data into training and validation sets
+temp = pd.DatetimeIndex(train['datetime'])
+training = train[temp.day <= 16]
+validation = train[temp.day > 16]
+
+param_grid = {'learning_rate': [0.1, 0.05, 0.01], 'max_depth':[10,15,20], 'min_samples_leaf': [3,5,10,20]}
+est = ensemble.GradientBoostingRegressor(n_estimators=500)
+
+gs_cv = GridSearchCV(est, param_grid, n_jobs=4).fit(training[features],training['log-count'])
+
+#best hyperparameter setting
+gs_cv.best_params_
+
+#Baseline error
+error_count = mean_absolute_error(validation['log-count'], gs_cv.predict(validation[features]))
+
+result = gs_cv.predict(test[features])
+result = np.expm1(result)
+df=pd.DataFrame({'datetime':test['datetime'], 'count':result})
+df.to_csv('results2.csv', index = False, columns = ['datetime', 'count'])
+
 
 
 
